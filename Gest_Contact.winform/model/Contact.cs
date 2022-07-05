fa38be52b7d0c05734a7776f8325e1b330d8d6b3
AZZIBROUCK <azby99720@gmail.com>
2022-07-05 04:52:39 +0000

    Ajoutez des fichiers projet.

---
+++ Gest_Contact.winform/model/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gest_Contact.winform.model
+{
+    internal class Contact
+    {
+        public int Id { get; set; }
+        public string NomComplet { get; set; }
+        public DateTime DateNaiss { get; set; }
+        public string Email { get; set; }
+        public string Tel { get; set; }
+        public string Genre { get; set; }
+        public string UrlPhoto { get; set; }
+
+        public Contact(string nomComplet, DateTime dateNaiss, string email, string tel, string genre, string urlPhoto)
+        {
+            this.NomComplet = nomComplet;
+            this.DateNaiss = dateNaiss;
+            this.Email = email;
+            this.Tel = tel;
+            this.Genre = genre;
+            this.UrlPhoto = urlPhoto;
+        }
+        public Contact() { }
+    }
+}
